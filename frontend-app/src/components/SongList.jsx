5adc0254b7fad6aede5d6df5b9efcaf1e6166770
new <2031134102@qq.com>
2025-06-01 18:28:26 +0800

    refactor(deps): 将 react-beautiful-dnd 替换为 @hello-pangea/dnd
    react-beautiful-dnd 已弃用，改用其维护分支版本 @hello-pangea/dnd 以获得持续支持和更新

--- frontend-app/src/components/SongList.jsx
+++ frontend-app/src/components/SongList.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+import { DragDropContext, Droppable, Draggable } from '@hello-pangea/dnd';
 import {usePlayer} from '../contexts/PlayerContext';
 import {useTheme} from '../contexts/ThemeContext';
 import {FavoriteIcon} from '../components/Icons';
